1aae0c3f2874da2e8c8f40a508b9a6a1f8fc7d9e
StevenSpiegl <steven.spiegl@gmail.com>
2022-04-25 17:07:34 +0100

    Closing #4: balance added

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,7 +1,11 @@
 class Oystercard
+  attr_reader :balance
+  
+  def initialize
+    @balance = 0
+  end
+
   def add_money
   end
 
-  def balance
-  end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -6,14 +6,27 @@ describe Oystercard do
   # assert
   # expect(subject.add_money).to eq
 
+  #plan to remove this test when refactoring once we have respond to money with 1 argument working
   it 'responds to add_money' do
     subject.add_money
     expect(subject).to respond_to :add_money
   end
-  
+
+  # it 'responds to add_money with one argument' do
+  #   expect(subject).to respond_to(:add_money).with(1).argument
+  # end
+
   it 'responds to balance' do
     subject.balance
     expect(subject).to respond_to :balance
+  end
+
+  it 'returns balance' do
+    expect(subject.balance).to eq(0)
+  end
+
+  it 'creates an instance of Oystercard' do
+    expect(subject).to be_instance_of(Oystercard)
   end
 
 end
