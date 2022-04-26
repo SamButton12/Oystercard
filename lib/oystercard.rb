d97d9a67a3edf082e7b20f236c127c0a50f4073b
Sam Button <sam_button_94@hotmail.co.uk>
2022-04-26 14:55:31 +0100

    Mid #8: added in_journey

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,10 +1,12 @@
 class Oystercard
   attr_reader :balance
 
+
   MAX_BALANCE = 90
 
   def initialize
     @balance = 0
+    @in_journey = false
   end
 
   def add_money(money)
@@ -15,10 +17,30 @@ class Oystercard
   def deduct_money(money)
     @balance -= money
   end
-  #single responsibility principle honoured by making max_balance_exceeded method instead of
-  #including @balance >= MAX_BALANCE in the fail method above
+
   def max_balance_exceeded
     @balance >= MAX_BALANCE
   end
 
+  def in_journey?
+    @in_journey
+  end
+
+  def touch_in
+    @in_journey = true
+  end
+
 end
+
+
+
+
+# def touch_in
+#   raise 'You cannot touch in twice' if @on_journey.empty? != true
+#   @on_journey.push "On Journey" if @on_journey.empty? == true
+# end
+
+# def touch_out
+#   raise 'You cannot touch out without touching in' if @on_journey.empty? == true
+#   @on_journey.pop if @on_journey.empty? != true
+# end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -15,7 +15,7 @@ describe Oystercard do
   describe '#add money' do
     it 'does not allow user to add more than maximum funds' do
       subject.add_money(90)
-      expect { subject.add_money(1) }.to raise_error 'funds cannot be added: maximum balance £90'
+      expect { subject.add_money(1) }.to raise_error("funds cannot be added: maximum balance £#{Oystercard::MAX_BALANCE}")
     end
   end
 
@@ -35,4 +35,25 @@ describe Oystercard do
   it 'responds to deduct_money with 1 argument' do
     expect(subject).to respond_to(:deduct_money).with(1).argument
   end
+  
+  it 'checks to see if oystercard is in journey' do
+    expect(subject.in_journey?).to be false
+  end
+
+  it 'when card touches in, in journey is changed to true' do
+    subject.touch_in
+    expect(subject).to be_in_journey
+  end
 end
+
+
+
+
+
+# it 'allows user to touch in' do
+#   expect(subject).to respond_to :touch_in
+# end
+
+# it 'allows user to touch out' do
+# expect(subject).to respond_to :touch_out
+# end
